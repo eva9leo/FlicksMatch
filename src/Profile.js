80c7c97d8f8f3528fa02fa6cabce1bed8d1b780b
Your Name <you@example.com>
2021-02-16 18:57:04 -0500

    updated profile UI

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { View, Text, Button, StyleSheet, Alert } from "react-native"
+import { View, Text, Button, StyleSheet, Alert, TouchableOpacity } from "react-native"
 import { auth } from "../firebaseConfig"
 import { AuthContext } from "../StateProvider"
 
@@ -14,8 +14,12 @@ export default function Profile() {
 
     return (
         <View style={styles.container}>
-            <Text>{currentUser?.displayName}</Text>
-            <Button title={"Log out"} onPress={logout}></Button>
+            <Text>{"Hello, " + currentUser?.displayName}</Text>
+            <View style={styles.startButtonContainer}>
+                <TouchableOpacity style={styles.buttonContainer} onPress={logout}>
+                    <Text style={styles.buttonText}>Log Out</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -25,6 +29,28 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#f2784b",
         width: "100%",
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center"
     },
+    buttonContainer: {
+        elevation: 8,
+        backgroundColor: "#009688",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        height: 40,
+        marginBottom: 10
+      },
+    buttonText: {
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+    },
+    startButtonContainer: {
+        position: "absolute",
+        bottom: 40,
+        width: "80%",
+    }
 });
