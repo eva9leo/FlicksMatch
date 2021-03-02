f37e9174e8a7c0bcb071abc17905d4456a101610
Tiansheng Zhou <32146093+eva9leo@users.noreply.github.com>
2021-03-02 08:31:12 -0500

    added place holder for media details

---
+++ src/components/MediaDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native'
+
+class MediaDetail extends Component {
+    render() {
+        // return ();
+    }
+
+}
+
+export default MediaDetail
